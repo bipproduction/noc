4e1367224be6ef8a222b088cb6b5c8d3db18602c
bipproduction <bip.production.js@gmail.com>
2023-09-08 14:15:34 +0800

    makuro-tools Fri Sep  8 14:15:34 WITA 2023

--- src/module/auth/login/fun/login.ts
+++ src/module/auth/login/fun/login.ts
@@ -4,11 +4,11 @@ import { sealData } from 'iron-session'
 export async function funLogin(data: any) {
     console.log(data)
     if (data.email === "ronald@bip.com" && data.password === "Bip_123") {
-        const name = await sealData("ronald", { password: process.env.PWD as string })
+        // const name = await sealData("ronald", { password: process.env.PWD as string })
 
         cookies().set({
             name: "noc_tkn",
-            value: name
+            value: "ronald"
         })
 
         return {
