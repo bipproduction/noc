747e0691534e07d05f4e4044a2b90bdcc124c384
bipproduction <bip.production.js@gmail.com>
2023-09-08 14:17:30 +0800

    makuro-tools Fri Sep  8 14:17:30 WITA 2023

--- src/app/noc/page.tsx
+++ src/app/noc/page.tsx
@@ -7,7 +7,7 @@ import Link from "next/link";
 export default function Page() {
     return <>
         <Flex p={"md"} gap={"lg"} justify={"space-between"}>
-            <Flex>
+            <Flex gap={"lg"}>
                 <Link href={'/noc/n1'} >
                     <UnstyledButton>Noc N1</UnstyledButton>
                 </Link>
